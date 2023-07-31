c2e2229cb40b57e86235628fa3842d49ebb0b807
iff <iff@ik.me>
2023-07-31 21:12:57 +0200

    feat: auto add nushell command

--- src/args.rs
+++ src/args.rs
@@ -1,3 +1,5 @@
+use std::io::prelude::*;
+
 pub fn handle_args() {
 	let args = std::env::args().collect::<Vec<String>>();
 	if args.len() > 1 {
@@ -22,13 +24,37 @@ pub fn handle_args() {
 			"nu" | "nush" | "nushell" => {
 				last_command = "(history | last).command";
 				alias = "\"\"";
-				println!(
+				let command = format!(
 					"with-env {{ _PR_LAST_COMMAND : {},\
 					_PR_ALIAS : {},\
 					_PR_SHELL : nu }} \
 					{{ {} }}",
 					last_command, alias, binary_path
 				);
+				println!("{}\n", command);
+				println!("Add following to your config file? (Y/n)");
+				let alias = format!("alias f = {}", command);
+				println!("{}", alias);
+				let mut input = String::new();
+				std::io::stdin().read_line(&mut input).unwrap();
+				match input.trim() {
+					"Y" | "y" | "" => {
+						let output = std::process::Command::new("nu")
+							.arg("-c")
+							.arg("echo $nu.config-path")
+							.output()
+							.expect("Failed to execute process");
+						let config_path= String::from_utf8_lossy(&output.stdout);
+						let mut file = std::fs::OpenOptions::new()
+							.write(true)
+							.append(true)
+							.open(config_path.trim())
+							.expect("Failed to open config file");
+
+						writeln!(file, "{}", alias).expect("Failed to write to config file");
+					},
+					"n" | _ => std::process::exit(0),
+				};
 				std::process::exit(0);
 			}
 			_ => {
