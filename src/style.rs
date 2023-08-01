b6b080043c3f428e4a01a818f9a4530b191fbf6c
iff <iff@ik.me>
2023-08-01 20:40:19 +0200

    fix: missing color in display

--- src/style.rs
+++ src/style.rs
@@ -21,9 +21,9 @@ pub fn highlight_difference(suggested_command: &str, last_command: &str) -> Stri
 	let mut highlighted = suggested_command.to_string();
 	for entry in &split_suggested_command {
 		if old_entries.contains(entry) {
-			highlighted = highlighted.replace(entry, &entry.cyan());
+			highlighted = highlighted.replace(entry, &entry.cyan().to_string());
 		} else {
-			highlighted = highlighted.replace(entry, &entry.red().bold());
+			highlighted = highlighted.replace(entry, &entry.red().bold().to_string());
 		}
 	}
 
