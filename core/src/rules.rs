a662d4db7f3a28304e79966309c030517d7a3c20
iff <iff@ik.me>
2026-03-31 21:18:12 +0200

    chore: cleaner code

--- core/src/rules.rs
+++ core/src/rules.rs
@@ -3,6 +3,7 @@ use pay_respects_parser::parse_rules;
 use pay_respects_utils::evals::*;
 
 pub fn match_pattern(executable: &str, data: &Data) -> Option<Vec<String>> {
+	// variables to be used by parsed rules
 	let error_msg = &data.error;
 	let error_lower = error_msg.to_lowercase();
 	let shell = &data.shell;
@@ -10,10 +11,12 @@ pub fn match_pattern(executable: &str, data: &Data) -> Option<Vec<String>> {
 	let executables = &data.executables;
 	let mut candidates = vec![];
 	let split = split_command(last_command);
+
+	// parse rules into rust code
 	parse_rules!("rules");
+
 	if candidates.is_empty() {
-		None
-	} else {
-		Some(candidates)
+		return None;
 	}
+	Some(candidates)
 }
