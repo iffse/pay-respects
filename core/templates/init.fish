351b4fa6d3c0fcc2bdd80052ad0f04faeb4b17d4
iff <iff@ik.me>
2026-04-04 04:16:00 +0200

    fix: fish inline mode init

--- core/templates/init.fish
+++ core/templates/init.fish
@@ -2,20 +2,19 @@ function {{ alias }} -d "Suggest fixes to the previous command"
 	eval $(_PR_LAST_COMMAND="$(builtin history | head -n 1)" _PR_ALIAS="$(alias)" _PR_SHELL="fish" "{{ binary_path }}")
 end
 
-function __pr_wrapper
+function __pr_inline
 	set input (commandline)
 
-	# export _PR_MODE="inline"
-	# export variables for the external program
-	set -x _PR_MODE "inline"
-	set output ($(_PR_LAST_COMMAND="$input" _PR_ALIAS="$(alias)" _PR_SHELL="fish" "{{ binary_path }}"))
+	set output $(_PR_MODE="inline" _PR_LAST_COMMAND="$input" _PR_ALIAS="$(alias)" _PR_SHELL="fish" "{{ binary_path }}")
 
 	if test -n "$output"
 		commandline --replace "$output"
 	end
 end
 
-bind \cx\cx __pr_wrapper
+for mode in default insert
+	bind -M $mode \cx\cx __pr_inline
+end
 
 {%if cnf %}
 if status is-interactive
