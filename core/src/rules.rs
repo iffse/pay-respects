c5b0bd8fb6201cb36f85c6aee12b5883a02b6d45
iff <iff@ik.me>
2026-04-24 02:19:11 +0200

    refactor: merge inline and matching suggestions

--- core/src/rules.rs
+++ core/src/rules.rs
@@ -1,11 +1,19 @@
 use crate::data::Data;
 use pay_respects_parser::{parse_inline_rules, parse_rules};
-use pay_respects_utils::evals::*;
+use pay_respects_utils::{evals::*, modes::Mode};
 
 #[allow(unused)]
 use crate::rules_function::{Functions::*, rules_function};
 
-pub fn match_pattern(executable: &str, data: &Data) -> Option<Vec<String>> {
+pub fn match_rule(executable: &str, data: &Data) -> Option<Vec<String>> {
+	use Mode::*;
+	match data.mode {
+		Inline => match_inline(executable, data),
+		_ => match_pattern(executable, data),
+	}
+}
+
+fn match_pattern(executable: &str, data: &Data) -> Option<Vec<String>> {
 	// variables to be used by parsed rules
 	let error_msg = &data.error;
 	let error_lower = error_msg.to_lowercase();
@@ -26,7 +34,7 @@ pub fn match_pattern(executable: &str, data: &Data) -> Option<Vec<String>> {
 
 #[allow(dead_code)]
 #[allow(unused)]
-pub fn match_inline(executable: &str, data: &Data) -> Option<Vec<String>> {
+fn match_inline(executable: &str, data: &Data) -> Option<Vec<String>> {
 	// variables to be used by parsed rules
 	// error variables are not used by inlines, they are here for reusing codes
 	let error_msg = &data.error;
